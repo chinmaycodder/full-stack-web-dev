614ebce0c3528c16c298fd362cacb1cecaea97a6
chinmay wadhwa <chinmaywadhwa01@gmail.com>
2023-09-02 16:53:12 +0530

    js

--- javascript/day1.js
+++ javascript/day1.js
@@ -1,5 +1,11 @@
-newFunction();
+let namastebtn = document.querySelector('button');
+namastebtn.addEventListener('click', inputmsg);
 
-function newFunction() {
-    console.log("hello world ");
+
+function inputmsg(){
+    let name = prompt('enter name of student');
+    namastebtn.textContent = 'roll no.1:' + name;
+
+
+
 }
---
+++ javascript/day2.js
@@ -0,0 +1,47 @@
+// this are the variables example for the javascript
+
+var a=78;
+var b="chinmay";
+var c="45.5";
+console.log(c);
+
+
+
+// this are the operators for the javascripts as folow
+
+// there are three types of operators
+//  1.unary operators it has single operands
+        c=-c;
+        console.log(c);
+
+//  2. binary operators= it has two operands
+c=456+56;
+// console.log(c);
+
+
+var num1=18;
+var num2=45;
+
+
+// arthematic operators in js
+console.log(" the value of num1+num2 is "+(num1+num2));
+console.log(" the value of num1-num2 is "+(num1-num2));
+console.log(" the value of num1*num2 is "+(num1*num2));
+console.log(" the value of num1%num2 is "+(num1%num2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// oprands = entities on which operators operates
+
+
+
